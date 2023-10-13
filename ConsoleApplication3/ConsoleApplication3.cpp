ec71e8efbedb5134999488675797685b4337b798
ChairmanMini <mister.tester.iv@mail.ru>
2023-10-13 21:47:11 +0300

    Задание от Миши

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 int main()
 {
-	int a = 3;
-	int& pa = a;
-	cout << "a = " << a << "\n" << "pa = " << pa << "\n";
-	pa += 2;
-	cout << "a = " << a << "\n" << "pa = " << pa << "\n";
+	int decade = 1488;
+	int *arr = new int[11];
+	int i = 1;
+	while (decade > 0)
+	{
+		arr[i] = decade % 2;
+		decade /= 2;
+		i++;
+	}
+	//arr[] = new int[i]
+	for (int i = 0; i < sizeof(arr); i++)
+	{
+			int temp = 0;
+			temp = arr[i];
+			arr[i] = arr[sizeof(arr) - 1 - i];
+			arr[sizeof(arr) - 1 - i] = temp;
+	}
+	for (int i = 0; i < sizeof(arr); i++)
+	{
+		cout << arr[i];
+	}
 
 }
